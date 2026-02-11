6103c22a495fc43d13ee43bc3a7b724c46796661
femoxmed <femoxmed@gmail.com>
2026-02-11 14:16:03 +0100

    use router to make code structure better

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
+import router from './routes/index.route';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
 const port = process.env.PORT || 3005;
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server listening on port ${port}`);
